01114c7b061b1727a67e4ab496ec39ba9f3b3096
ichi-tatsuki <147365572+ichi-tatsuki@users.noreply.github.com>
2023-11-26 02:24:15 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const menuButton = document.getElementById('menuButton');
-const sidebar = document.getElementById('sidebar');
-const overlay = document.getElementById('overlay');
-const content = document.getElementById('content');
 const romaf = document.getElementById('roma');
 const userf = document.getElementById('user');
-const seat = document.getElementById('seatimg');
+const cc_pos = document.getElementById('cc-pos');
 const id = window.location.href.split("=").pop();
 const url = "https://script.google.com/macros/s/AKfycbziaNMSmmpQI00wLbfOklS0-d_zzuth5Rt8biB0DZkR2VwoNtD4EJD7UMz5l5hG7h0/exec";
-const meibo = {kawaihirokazu:{'name':"河合洋寿",'roma':"Kawai",'role':"新郎"},akitomohiro:{'name':"安藝友宏",'roma':"Aki",'role':"新郎"},matsudatakanori:{'name':"松田貴教",'roma':"Matsuda",'role':"新郎(親族)"},matsudatoshiyo:{'name':"松田年代",'roma':"Matsuda",'role':"新郎(親族)"},matsudahiromu:{'name':"松田弘",'roma':"Matsuda",'role':"新郎(親族)"},ichinoseami:{'name':"市之瀬愛海",'roma':"Ichinose",'role':"新郎(親族)"},ichinosetomomi:{'name':"市之瀬智美",'roma':"Ichinose",'role':"新郎(親族)"},ichinosehideki:{'name':"市之瀬秀樹",'roma':"Ichinose",'role':"新郎(親族)"},omurotakaho:{'name':"大室高帆",'roma':"Omuro",'role':"新郎"},itokohei:{'name':"伊藤昂平",'roma':"Ito",'role':"新郎"},matsuokanatsumi:{'name':"松岡夏美",'roma':"Matsuoka",'role':"新郎"},kitokosuke:{'name':"鬼頭幸助",'roma':"Kito",'role':"新郎"},kumazawaakira:{'name':"熊澤晶",'roma':"Kumazawa",'role':"新郎"},masubuchitomonori:{'name':"増渕那典",'roma':"Masubuchi",'role':"新郎"},chibashoya:{'name':"千葉翔也",'roma':"Chiba",'role':"新郎"},machidayuto:{'name':"町田裕斗",'roma':"Machida",'role':"新郎"},sawadatakuya:{'name':"澤田拓弥",'roma':"Sawada",'role':"新郎"},nakazatoriki:{'name':"中里力",'roma':"Nakazato",'role':"新郎"},uchidatadashi:{'name':"内田賢司",'roma':"Uchida",'role':"新郎"},horiekenta:{'name':"堀江健太",'roma':"Horie",'role':"新婦"},haradatakuya:{'name':"原田拓弥",'roma':"Harada",'role':"新婦"},kondohikaru:{'name':"近藤光",'roma':"Kondo",'role':"新婦"},takenakamayu:{'name':"竹中茉佑",'roma':"Takenaka",'role':"新婦"},mizunonatsuki:{'name':"水野夏希",'roma':"Mizuno",'role':"新婦"},tatewakireina:{'name':"立脇鈴菜",'roma':"Tatewaki",'role':"新婦"},suzukikei:{'name':"鈴木景",'roma':"Suzuki",'role':"新婦"},ishizukamasateru:{'name':"石塚匡皓",'roma':"Ishizuka",'role':"新婦"},naritayumika:{'name':"成田弓夏",'roma':"Narita",'role':"新婦"},soneharatatsuki:{'name':"曽根原樹",'roma':"Sonehara",'role':"新婦"},odachikako:{'name':"小田千佳子",'roma':"Oda",'role':"新婦"},takikanari:{'name':"瀧加菜里",'roma':"Taki",'role':"新婦"},miurakanako:{'name':"三浦可南子",'roma':"Miura",'role':"新婦"},kamemuramiki:{'name':"亀村美樹",'roma':"Kamemura",'role':"新婦"},katoshiho:{'name':"加藤詩歩",'roma':"Kato",'role':"新婦"},okanomomoka:{'name':"岡野百花",'roma':"Okano",'role':"新婦"},sononoyuki:{'name':"園野有希",'roma':"Sonono",'role':"新婦"},nakamuramadoka:{'name':"中村円香",'roma':"Nakamura",'role':"新婦"},maedatakeshi:{'name':"前田健",'roma':"Maeda",'role':"新婦(親族)"},maedanaoko:{'name':"前田斉子",'roma':"Maeda",'role':"新婦(親族)"},maedaharuomi:{'name':"前田啓臣",'roma':"Maeda",'role':"新婦(親族)"},maedaakihito:{'name':"前田照仁",'roma':"Maeda",'role':"新婦(親族)"},maedanatsuki:{'name':"前田奈槻",'roma':"Maeda",'role':"新婦(親族)"},sudomasanori:{'name':"須藤雅教",'roma':"Sudo",'role':"新婦(親族)"},sudonoriko:{'name':"須藤典子",'roma':"Sudo",'role':"新婦(親族)"},sudoreiko:{'name':"須藤怜子",'roma':"Sudo",'role':"新婦(親族)"},sudomaiko:{'name':"須藤舞子",'roma':"Sudo",'role':"新婦(親族)"},horikawaetsuji:{'name':"堀川悦二",'roma':"Horikawa",'role':"新婦(親族)"},horikawayoshiko:{'name':"堀川美子",'roma':"Horikawa",'role':"新婦(親族)"},horikawamanabu:{'name':"堀川学",'roma':"Horikawa",'role':"新婦(親族)"},horikawamasakatsu:{'name':"堀川将克",'roma':"Horikawa",'role':"新婦(親族)"},matsudatomoko:{'name':"松田知子",'roma':"Matsuda",'role':"新婦(親族)"},kamemotoyukiko:{'name':"亀元佑喜子",'roma':"Kamemoto",'role':"新婦(親族)"},kamemotokeita:{'name':"亀元奎汰",'roma':"Kamemoto",'role':"新婦(親族)"},kamemotoyasutaka:{'name':"亀元康敬",'roma':"Kamemoto",'role':"新婦(親族)"},kawaharakentaro:{'name':"川原健太郎",'roma':"Kawahara",'role':"新婦(親族)"},kawaharayoshiko:{'name':"川原佳子",'roma':"Kawahara",'role':"新婦(親族)"}}
+const meibo = {akitomohiro:{'name': "安藝 友宏", 'roma': "Aki", 'pos': "a5"}, ishizukamasateru:{'name': "石塚 匡皓", 'roma': "Ishizuka", 'pos': "g3"}, ichinoseami:{'name': "市之瀬 愛海", 'roma': "Ichinose", 'pos': "e1"}, ichinosetomomi:{'name': "市之瀬 智美", 'roma': "Ichinose", 'pos': "e3"}, ichinosehideki:{'name': "市之瀬 秀樹", 'roma': "Ichinose", 'pos': "e2"}, itokohei:{'name': "伊藤 昂平", 'roma': "Ito", 'pos': "a4"}, uchidatadashi:{'name': "内田 賢司", 'roma': "Uchida", 'pos': "b4"}, omurotakaho:{'name': "大室 高帆", 'roma': "Omuro", 'pos': "a1"}, okanomomoka:{'name': "岡野 百花", 'roma': "Okano", 'pos': "c1"}, odachikako:{'name': "小田 千佳子", 'roma': "Oda", 'pos': "g4"}, katoshiho:{'name': "加藤 詩歩", 'roma': "Kato", 'pos': "c2"}, kamemuramiki:{'name': "亀村 美樹", 'roma': "Kamemura", 'pos': "c7"}, kamemotokeita:{'name': "亀元 奎汰", 'roma': "Kamemoto", 'pos': "i4"}, kamemotoyasutaka:{'name': "亀元 康敬", 'roma': "Kamemoto", 'pos': "i1"}, kamemotoyukiko:{'name': "亀元 佑喜子", 'roma': "Kamemoto", 'pos': "i2"}, kawaihirokazu:{'name': "河合 洋寿", 'roma': "Kawai", 'pos': "b5"}, kawaharakentaro:{'name': "川原 健太郎", 'roma': "Kawahara", 'pos': "i5"}, kawaharayoshiko:{'name': "川原 佳子", 'roma': "Kawahara", 'pos': "i6"}, kitokosuke:{'name': "鬼頭 幸助", 'roma': "Kito", 'pos': "b2"}, kumazawaakira:{'name': "熊澤 晶", 'roma': "Kumazawa", 'pos': "b1"}, kondohikaru:{'name': "近藤 光", 'roma': "Kondo", 'pos': "d3"}, sawadatakuya:{'name': "澤田 拓弥", 'roma': "Sawada", 'pos': "b7"}, suzukikei:{'name': "鈴木 景", 'roma': "Suzuki", 'pos': "g1"}, sudonoriko:{'name': "須藤 典子", 'roma': "Sudo", 'pos': "h8"}, sudomaiko:{'name': "須藤 舞子", 'roma': "Sudo", 'pos': "h6"}, sudomasanori:{'name': "須藤 雅教", 'roma': "Sudo", 'pos': "h9"}, sudoreiko:{'name': "須藤 怜子", 'roma': "Sudo", 'pos': "h7"}, soneharatatsuki:{'name': "曽根原 樹", 'roma': "Sonehara", 'pos': "g6"}, sononoyuki:{'name': "園野 有希", 'roma': "Sonono", 'pos': "c5"}, takikanari:{'name': "瀧 加菜里", 'roma': "Taki", 'pos': "c6"}, takenakamayu:{'name': "竹中 茉佑", 'roma': "Takenaka", 'pos': "d1"}, tatewakireina:{'name': "立脇 鈴菜", 'roma': "Tatewaki", 'pos': "g2"}, chibashoya:{'name': "千葉 翔也", 'roma': "Chiba", 'pos': "b6"}, nakazatoriki:{'name': "中里 力", 'roma': "Nakazato", 'pos': "a6"}, nakamuramadoka:{'name': "中村 円香", 'roma': "Nakamura", 'pos': "c4"}, naritayumika:{'name': "成田 弓夏", 'roma': "Narita", 'pos': "g5"}, haradatakuya:{'name': "原田 拓弥", 'roma': "Harada", 'pos': "d4"}, horiekenta:{'name': "堀江 健太", 'roma': "Horie", 'pos': "d5"}, horikawaetsuji:{'name': "堀川 悦二", 'roma': "Horikawa", 'pos': "f4"}, horikawamasakatsu:{'name': "堀川 将克", 'roma': "Horikawa", 'pos': "f1"}, horikawamanabu:{'name': "堀川 学", 'roma': "Horikawa", 'pos': "f3"}, horikawayoshiko:{'name': "堀川 美子", 'roma': "Horikawa", 'pos': "f2"}, maedaakihito:{'name': "前田 照仁", 'roma': "Maeda", 'pos': "h2"}, maedatakeshi:{'name': "前田 健", 'roma': "Maeda", 'pos': "h5"}, maedanaoko:{'name': "前田 斉子", 'roma': "Maeda", 'pos': "h4"}, maedanatsuki:{'name': "前田 奈槻", 'roma': "Maeda", 'pos': "h1"}, maedaharuomi:{'name': "前田 啓臣", 'roma': "Maeda", 'pos': "h3"}, masubuchitomonori:{'name': "増渕 那典", 'roma': "Masubuchi", 'pos': "a2"}, machidayuto:{'name': "町田 裕斗", 'roma': "Machida", 'pos': "a3"}, matsuokanatsumi:{'name': "松岡 夏美", 'roma': "Matsuoka", 'pos': "b3"}, matsudatakanori:{'name': "松田 貴教", 'roma': "Matsuda", 'pos': "e6"}, matsudatoshiyo:{'name': "松田 年代", 'roma': "Matsuda", 'pos': "e5"}, matsudatomoko:{'name': "松田 知子", 'roma': "Matsuda", 'pos': "i3"}, matsudahiromu:{'name': "松田 弘", 'roma': "Matsuda", 'pos': "e4"}, miurakanako:{'name': "三浦 可南子", 'roma': "Miura", 'pos': "c3"}, mizunonatsuki:{'name': "水野 夏希", 'roma': "Mizuno", 'pos': "d2"}}
+const pos = {a1: [14, 28], a2: [13, 38], a3: [14, 48], a4: [24, 28], a5: [26, 38], a6: [24, 48], b1: [35, 28], b2: [33, 38], b3: [34, 48], b4: [43.5, 28], b5: [46, 38], b6: [44, 48], b7: [39, 52], c1: [54, 28], c2: [53, 38], c3: [54, 48], c4: [64, 28], c5: [65.5, 38], c6: [64, 48], c7: [59, 52], d1: [75, 28], d2: [72.5, 40], d3: [79, 52], d4: [83, 28], d5: [85.5, 40], e1: [5, 69], e2: [3.5, 79], e3: [6, 89], e4: [14.5, 69], e5: [16, 79], e6: [14, 89], f1: [25.5, 69], f2: [25.5, 88.5], f3: [34.5, 69], f4: [34.5, 88.5], g1: [45.5, 69], g2: [43.5, 79], g3: [45.5, 89], g4: [54, 69], g5: [56, 79], g6: [54, 89], h1: [68, 66], h2: [64, 71], h3: [63, 79], h4: [65.5, 87], h5: [70, 92], h6: [72, 66], h7: [75.5, 71], h8: [76, 79], h9: [74.5, 87], i1: [85, 69], i2: [83, 79], i3: [86, 89], i4: [94.5, 69], i5: [96, 79], i6: [94, 89]}
 
 romaf.textContent = meibo[id].roma
 userf.textContent = meibo[id].name
+cc_pos.style.left = pos[meibo[id].pos][0] + "%";
+cc_pos.style.top = pos[meibo[id].pos][1] + "%";
+
 
 function writeToSpreadsheet(answer) {
     var data = {
@@ -28,67 +28,7 @@ function writeToSpreadsheet(answer) {
     };
 
     fetch(url, options)
-            .then(response => response.json()) // 応答をJSON形式に解析
-            .then(data => console.log(data)) // 応答データをコンソールに表示
-            .catch(error => console.error('Error:', error)); // エラーハンドリング
+        .then(response => response.json()) // 応答をJSON形式に解析
+        .then(data => console.log(data)) // 応答データをコンソールに表示
+        .catch(error => console.error('Error:', error)); // エラーハンドリング
 }
-function openSidebar() {
-    sidebar.style.left = '0';
-    overlay.style.display = 'block';
-    document.body.style.overflow = 'hidden'; /* スクロールを無効化 */
-}
-
-function closeSidebar() {
-    sidebar.style.left = '-250px';
-    overlay.style.display = 'none';
-    document.body.style.overflow = 'auto'; /* スクロールを有効化 */
-}
-
-function loadTopPage() {
-    content.innerHTML = `
-                <h1 style="font-family:cursive">`+ user + `-San<br>Welcome to <br> Tatsuki & Sakiko 's Wedding Party</h1>
-            `;
-    closeSidebar();
-    header.style.display = 'none';
-    // body.style.background-color=null;
-    document.body.style.backgroundImage = "url(https://drive.google.com/uc?id=1lT3eqossuwzjPbpvTQ4TonSOTF7wV4lh)";
-}
-
-function loadSeatPage() {
-    content.innerHTML = seatHtml;
-    closeSidebar();
-    header.style.display = 'block';
-    header.style.backgroundImage = "url(https://drive.google.com/uc?id=1eonxUWg3Bk3EaunGqelRpSKVOEUv3mbJ)";
-    headerTitle.textContent = "席次";
-    document.body.style.backgroundImage = "none";
-}
-
-function loadProfilePage() {
-    content.innerHTML = profileHtml;
-    closeSidebar();
-    header.style.display = 'block';
-    header.style.backgroundImage = "url(https://drive.google.com/uc?id=1eonxUWg3Bk3EaunGqelRpSKVOEUv3mbJ)";
-    headerTitle.textContent = "Profile";
-    document.body.style.backgroundImage = "none";
-}
-
-function loadMenuPage() {
-    content.innerHTML = menuHtml;
-    closeSidebar();
-    header.style.display = 'block';
-    header.style.backgroundImage = "url(https://drive.google.com/uc?id=198cEPypWrpHRwLoet4LOkkIB2AkVoViA)";
-    headerTitle.textContent = "Menu";
-    document.body.style.backgroundImage = "none";
-}
-
-function loadQuizPage() {
-    content.innerHTML = quizHtml;
-    closeSidebar();
-    header.style.display = 'block';
-    header.style.backgroundImage = "url(https://drive.google.com/uc?id=198cEPypWrpHRwLoet4LOkkIB2AkVoViA)";
-    headerTitle.textContent = "Quiz";
-    document.body.style.backgroundImage = "none";
-}
-
-// menuButton.addEventListener('click', openSidebar);
-// overlay.addEventListener('click', closeSidebar);
